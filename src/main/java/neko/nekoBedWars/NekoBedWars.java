d0367155ffce585e7e51d76558ab32607513bd5f
Nyanyagulugulu <qianxun114514@gmail.com>
2025-10-23 12:13:30 +0800

    重写

---
+++ src/main/java/neko/nekoBedWars/NekoBedWars.java
@@ -0,0 +1,17 @@
+package neko.nekoBedWars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NekoBedWars extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
